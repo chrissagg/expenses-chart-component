af8a980777c61271a4dcc312e3edd63e98cf59a0
chrissagg <chriss.agg@outlook.com>
2022-07-04 21:43:04 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,36 @@
+// import JSON from data.json file
+const data = fetch('./data.json')
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+    return data;
+  })
+  .catch(error => {console.log(error)})
+
+// convert data into array
+let chartContainer = document.querySelector('.chart-container');
+
+// Map throught data and populate chart
+data.then(data => {
+  data.forEach(item => {
+    let chart = document.createElement('div');
+    chart.classList.add('chart');
+    const height = item.amount/7;
+    let value =""
+    if(item.day == "wed"){
+      value=`<div class="chart-value active" style="--height:${height}em"></div>`
+    } else {
+      value=`<div class="chart-value" style="--height:${height}em"></div>`
+    }
+   
+    chart.innerHTML = `
+    <div class="chart-wrapper">
+      ${value}
+    </div>  
+    <div class="chart-title">${item.day}</div>
+    
+    `;
+    chartContainer.appendChild(chart);
+  })
+})
+
